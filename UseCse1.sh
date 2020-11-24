1e8419bb16f84c3e4c948c59a6a23621e5ad6db6
Jayakansal <21jayajindal@gmail.com>
2020-11-24 16:03:30 +0530

    This is for PIN code

---
+++ UseCse1.sh
@@ -0,0 +1,9 @@
+echo "Welcome to the pattern execise"
+read -p "enter Postal Index Number:" PostalIndexNumber
+PostalIndexNumberpat="([0-9]{6})"
+if(( $PostalIndexNumber == $PostalIndexNumberpat ))
+then
+echo "yes"
+else
+echo "no"
+fi
