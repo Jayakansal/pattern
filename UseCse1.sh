506935113c824d9c1d7c7202f39e95f857ac6da8
Jayakansal <21jayajindal@gmail.com>
2020-11-24 16:20:31 +0530

    This is for PIN code starting from Special Character

--- UseCse1.sh
+++ UseCse1.sh
@@ -1,5 +1,14 @@
 echo "Welcome to the pattern execise"
 read -p "enter Postal Index Number:" PostalIndexNumber
+PostalIndexNumberpat="([0-9]{6})"
+if(( $PostalIndexNumber == $PostalIndexNumberpat ))
+then
+echo "yes"
+else
+echo "no"
+fi
+echo "Welcome to the pattern execise"
+read -p "enter Postal Index Number:" PostalIndexNumber
 PostalIndexNumberpat="(^[a-z A-Z]*\$[0-9]{6})"
 if(( $PostalIndexNumber == $PostalIndexNumberpat ))
 then
