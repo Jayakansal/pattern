79eea852291fe9b5281daf1b4f256cac131a9047
Jayakansal <21jayajindal@gmail.com>
2020-11-24 16:50:38 +0530

    This is to add the special character at the end

--- UseCse1.sh
+++ UseCse1.sh
@@ -1,6 +1,6 @@
 echo "Welcome to the pattern execise"
 read -p "enter Postal Index Number:" PostalIndexNumber
-PostalIndexNumberpat="([0-9]{6})"
+PostalIndexNumberpat=(^[0-9]{6}[a-z A-Z]*[\$@+-]$)
 if(( $PostalIndexNumber == $PostalIndexNumberpat ))
 then
 echo "yes"
